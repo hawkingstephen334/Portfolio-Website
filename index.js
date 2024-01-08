002d793df9ba2c82f72b56630ddeda6ec3662d97
hawkingstephen334 <155753058+hawkingstephen334@users.noreply.github.com>
2024-01-08 21:19:04 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// active menu//////////////////////////////////////////////
+
+
+
+// let menuLi = document.querySelectorAll('header ul li a');
+// let section = document.querySelectorAll('section');
+
+
+// function activeMenu(){
+//     let len = section.length;
+//     while (--len && window.scrollY + 97 <section[len].offsetTop){}
+//     menuLi.forEach (sec => sec.classList.remove("active"));
+//     menuLi[len].classList.add("active");
+// }
+
+
+// activeMenu();
+// window.addEventListener("scroll", activeMenu);
